35baba1a5692ba57b1e3b3bb1835dbf1cb2521df
Peter L <peterapps@users.noreply.github.com>
2017-08-29 16:10:26 -0400

    Add placeholders

--- email.php
+++ email.php
@@ -43,24 +43,24 @@ if(isset($_GET['send']) && isset($_POST['to']) && isset($_POST['from']) && isset
 			<table border="0">
 				<tr>
 					<td>To: </td>
-					<td><input type="text" name="to"></td>
+					<td><input type="email" name="to" placeholder="jane_doe@example.com"></td>
 				</tr>
 				
 				<tr>
 					<td>Fake From Email: </td>
-					<td><input type="text" name="from"></td>
+					<td><input type="email" name="from" placeholder="john_doe@example.com"></td>
 				</tr>
 				<tr>
 					<td>Fake From Name: </td>
-					<td><input type="text" name="fromname"></td>
+					<td><input type="text" name="fromname" placeholder="John Doe"></td>
 				</tr>
 				<tr>
 					<td>Subject: </td>
-					<td><input type="text" name="subject"></td>
+					<td><input type="text" name="subject" placeholder="Happy Birthday!"></td>
 				</tr>
 				<tr>
 					<td>Message: </td>
-					<td><textarea name="message"></textarea></td>
+					<td><textarea name="message" placeholder="Hope you're having a good birthday, sis."></textarea></td>
 				</tr>
 				<tr>
 					<td colspan="2">
